c4b0f8b762870025d5e8c2ae196a3e42c1b5d623
Tedid <teodorgenov06@gmail.com>
2026-05-12 17:03:48 +0300

    Added two subclasses of Discount and refined them

--- include/Objects/Discounts.hpp
+++ include/Objects/Discounts.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
-class Discount{
-    int discountID;     //unique
+#include <string>
+
+#include "Fragrance.hpp"
+
+class Discount
+{
+protected:
+    int discountID; // unique
     float discountPercent;
 
-    public:
+public:
     Discount(int id, float percent);
+    virtual ~Discount() = default;
 
-    void apply();
+    virtual float apply(const Fragrance &frag) const;
+};
+
+class BonusDiscount : public Discount
+{
+    float bonus;
+
+public:
+    BonusDiscount(int id, float percent, float bonus);
+
+    float apply(const Fragrance &frag) const override;
+};
+
+class BrandDiscount : public Discount
+{
+    std::string brandName;
+
+public:
+    BrandDiscount(int id, float percent, const std::string &brandName);
+    float apply(const Fragrance &frag) const override;
 };
