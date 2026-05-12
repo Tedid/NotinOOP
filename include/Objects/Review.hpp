8d038e1659dfea2de7447e56a9f5426367cb09ff
Tedid <teodorgenov06@gmail.com>
2026-05-12 14:53:09 +0300

    Update Review.hpp

--- include/Objects/Review.hpp
+++ include/Objects/Review.hpp
@@ -10,5 +10,5 @@ class Review{
     int rating;             // between 0 and 5
 
     public:
-    Review(std::string name, int userID, std::string comment, int rating);
+    Review(const std::string& name, int userID, const std::string& comment, int rating);
 };
