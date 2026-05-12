fb684f68812cd585f633c554fb31cb705ca0241a
Tedid <teodorgenov06@gmail.com>
2026-05-12 18:52:32 +0300

    File formatted
    Pressed Shift+Ctrl+I

--- include/NotinOOP.hpp
+++ include/NotinOOP.hpp
@@ -9,44 +9,45 @@
 #include "Objects/Discounts.hpp"
 #include "Objects/Review.hpp"
 
-class NotinOOP{
-    //Storing pointers because of subclasses:
-    std::vector<User*> users;
+class NotinOOP
+{
+    // Storing pointers because of subclasses:
+    std::vector<User *> users;
     std::vector<Fragrance> catalog;
     std::vector<Purchase> purchases;
     std::vector<Review> reviews;
-    //std::vector<Discount*> vouchers;  //I don't think I need to store these  here
+    // std::vector<Discount*> vouchers;  //I don't think I need to store these  here
 
-    User* activeUser;
+    User *activeUser;
 
     void processCommand(const std::string &command);
 
-    //User handling:
+    // User handling:
     void handleRegister(const std::string &name, const std::string &pass);
     void handleLogin(const std::string &name, const std::string &pass);
 
-    //Buyer handling:
+    // Buyer handling:
     void handleAddToBalance(float amount);
-    void handleAddToWishlist(const std::string& fragranceName);
-    void handleRemoveFromWishlist(const std::string& fragranceName);
-    void handleAddToCart(const std::string& fragranceName);
-    void handleRemoveFromCart(const std::string& fragranceName);
+    void handleAddToWishlist(const std::string &fragranceName);
+    void handleRemoveFromWishlist(const std::string &fragranceName);
+    void handleAddToCart(const std::string &fragranceName);
+    void handleRemoveFromCart(const std::string &fragranceName);
     void handleViewCart() const;
     void handleViewBought() const;
     void handleViewPurchases() const;
     void handleRecommend();
     void handleCheckout();
     void handleCancelPurchase(int purchaseId);
-    void handleMakeReview(const std::string& fragranceName, double rating, const std::string& comment);
+    void handleMakeReview(const std::string &fragranceName, double rating, const std::string &comment);
 
-    //Admin handling:
-    void handleBlockUser(const std::string& username);
-    void handleCreateFragrance(const std::string& name, const std::string& brand, float price, const std::string& ingredientsList);
-    void handleAddQuantity(const std::string& fragranceName, int quantity);
+    // Admin handling:
+    void handleBlockUser(const std::string &username);
+    void handleCreateFragrance(const std::string &name, const std::string &brand, float price, const std::string &ingredientsList);
+    void handleAddQuantity(const std::string &fragranceName, int quantity);
     void handleDeliverPurchase(int purchaseId);
     void handleRemoveReview(int fragranceId, int reviewId);
 
-    public:
+public:
     NotinOOP();
     ~NotinOOP();
 
