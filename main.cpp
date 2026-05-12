d7d5054c122cac41a4f916e600eee962f1b425a4
Tedid <teodorgenov06@gmail.com>
2026-05-12 18:59:05 +0300

    Finish development of main.cpp
    Absolute cinema

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
-test
+#include <iostream>
+
+#include "NotinOOP.hpp"
+
+int main() {
+    try {
+        NotinOOP engine;
+        engine.run();
+    } 
+    catch (const std::exception& e) {
+        std::cerr << "Critical Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
