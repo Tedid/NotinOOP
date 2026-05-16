2d298ee26bb20b445000550b4e0ba5b2bdf3e442
Tedid <teodorgenov06@gmail.com>
2026-05-16 16:32:38 +0300

    Added default values to variables

--- include/Objects/Review.hpp
+++ include/Objects/Review.hpp
@@ -7,8 +7,8 @@ class Review
     size_t reviewID;        // unique
     std::string fragranceName;
     size_t userID;
-    std::string comment; // <=1000 characters
-    int rating;          // between 0 and 5
+    std::string comment = ""; // <=1000 characters
+    int rating = 0;          // between 0 and 5
 
 public:
     Review(size_t ID, const std::string &name, size_t userID, const std::string &comment, int rating);
