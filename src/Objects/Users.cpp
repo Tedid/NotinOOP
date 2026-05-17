9e310bf5409f93dc158f8eaad7d34dfbd0de35a4
Tedid <teodorgenov06@gmail.com>
2026-05-17 12:37:32 +0300

    Created Users.cpp and defined some methods
    I did all the changes to printFragrancesByType because of this use. I didn't want to reuse code.

---
+++ src/Objects/Users.cpp
@@ -0,0 +1,60 @@
+#include "Objects/Users.hpp"
+#include "util/utils.hpp"
+
+#include <iostream>
+
+User::User(size_t id, const std::string &name, const std::string &pass)
+{
+    userID = id;
+    username = name;
+    password = pass;
+}
+
+size_t User::getUserID() const
+{
+    return userID;
+}
+
+std::string User::getUsername() const
+{
+    return username;
+}
+
+std::string User::getPassword() const
+{
+    return password;
+}
+
+Buyer::Buyer(size_t id, const std::string &name, const std::string &pass, float balance) : User(id, name, pass)
+{
+    this->balance = balance;
+}
+
+void Buyer::addToBalance(float money)
+{
+    balance += money;
+}
+
+void Buyer::addToWishlist(const Fragrance &fragrance)
+{
+    wishlist.push_back(fragrance);
+}
+
+void Buyer::removeFromWishlist(const Fragrance &fragrance)
+{
+    for (int i = 0; i < wishlist.size(); i++)
+    {
+        if (fragrance == wishlist[i])
+        {
+            wishlist.erase(wishlist.begin() + i);
+            break;
+        }
+    }
+}
+
+void Buyer::viewWishlist() const
+{
+    std::string message = "Wishlisted items: ";
+    std::cout << message;
+    Utils::printFragrancesByType(wishlist, message.size());
+}
